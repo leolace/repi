370cd0e999e9a28793ee1bed3b5ad0dee635624f
leolace <lgonsalez02@gmail.com>
2024-11-10 17:04:34 -0300

    feat: update db

--- packages/server/src/db-client.ts
+++ packages/server/src/db-client.ts
@@ -4,7 +4,10 @@ import { env } from "./env.ts";
 const config: pg.ClientConfig = {
   user: env.DB_USER,
   password: env.DB_PASSWORD,
-  database: env.DB_DATABASE,
+  host: env.DB_HOST,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 };
 
 const dbClient = new pg.Client(config);
--- packages/server/src/env.ts
+++ packages/server/src/env.ts
@@ -2,5 +2,7 @@ export const env = {
   DB_USER: Deno.env.get("DB_USER"),
   DB_PASSWORD: Deno.env.get("DB_PASSWORD"),
   DB_DATABASE: Deno.env.get("DB_DATABASE"),
+  DB_HOST: Deno.env.get("DB_HOST"),
+  DB_URL: Deno.env.get("DB_URL"),
   PORT: Deno.env.get("SERVER_PORT"),
 };
